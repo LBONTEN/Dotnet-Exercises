139887ee5ea6bab0f47815b05ae5f8b01973dc90
Lenny  Bontenakel <lennyb.0908@gmail.com>
2026-02-06 12:44:51 +0100

    solution for day 2 added

---
+++ Dotnet-Exercises/AoC_Day2/IDValidator.cs
@@ -0,0 +1,57 @@
+
+//  Solution for Advent of Code day 2 
+// https://adventofcode.com/2025/day/2
+
+class IDValidator()
+{
+    public static List<(long start, long end)> ParseInput(string fileName)
+    {
+        List<(long start, long end)> idRanges = new List<(long start, long end)>();
+        string text = File.ReadAllText(fileName);
+        string[] idRangeInputs = text.Split(',');
+        foreach (string input in idRangeInputs)
+        {
+            string[] splitInput = input.Split('-');
+            idRanges.Add((Convert.ToInt64(splitInput[0]), Convert.ToInt64(splitInput[1])));
+        }
+        return idRanges;
+    }
+
+    public static long ValidateIds(List<(long start, long end)> idRanges)
+    {
+        List<long> invalidIds = new List<long>();
+        foreach ((long start, long end) range in idRanges)
+        {
+            for (long i = range.start; i < range.end + 1; i++)
+            {
+                if (CheckForRepeatedSequence(i))
+                {
+                    invalidIds.Add(i);
+                }
+            }
+        }
+        long value = invalidIds.Sum();
+        return value;
+    }
+
+    private static bool CheckForRepeatedSequence(long input)
+    {
+        string inputAsString = Convert.ToString(input);
+        for (int i = 1; i < inputAsString.Length; i++)
+        {
+            if (inputAsString.Length % i == 0)
+            {
+                string[] sequence = inputAsString.Chunk(i)
+                    .Select(element => new string(element))
+                    .ToArray();
+
+                int distinct = sequence.Distinct().Count();
+                if (distinct == 1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- Dotnet-Exercises/Program.cs
+++ Dotnet-Exercises/Program.cs
@@ -1,26 +1,28 @@
 ﻿
-Console.WriteLine("Starting now.");
-
-
 // Day 1 
 Dial dial = new Dial();
 
 List<(char, int)> instructions = new List<(char, int)>();
-string fileName = "Aoc_Day1/input.txt";
-instructions = Dial.ParseInstructions(fileName);
+string fileNameDay1 = "Aoc_Day1/input.txt";
+instructions = Dial.ParseInstructions(fileNameDay1);
 int result = dial.SolveInstructions(instructions);
 
-Console.WriteLine("Result is:" + Convert.ToString(result));
+Console.WriteLine("Result 1 is:" + Convert.ToString(result));
 
 // Day 1 Part 2
 DialMKII newDial = new DialMKII();
 
 List<(char, int)> instructions2 = new List<(char, int)>();
-string fileName2 = "Aoc_Day2/input.txt";
-instructions2 = DialMKII.ParseInstructions(fileName2);
+instructions2 = DialMKII.ParseInstructions(fileNameDay1);
 int result2 = newDial.SolveInstructions(instructions2);
 
-Console.WriteLine("Result is:" + Convert.ToString(result2));
+Console.WriteLine("Result 2 is:" + Convert.ToString(result2));
 
+// Day 2 
+string fileNameDay2 = "Aoc_Day2/input.txt";
+List<(long, long)> idRanges = IDValidator.ParseInput(fileNameDay2);
+long result3 = IDValidator.ValidateIds(idRanges);
+
+Console.WriteLine("Result day 2 is:" + Convert.ToString(result3));
 
 
